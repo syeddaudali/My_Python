245abb5c7a3ed6deae58d832e7efc67e125e6c29
Syed Daud Ali <92268781+syeddaudali@users.noreply.github.com>
2023-08-03 14:52:06 +0500

    Add files via upload

---
+++ 5. Chapter 5/1.py
@@ -0,0 +1,60 @@
+'''daud={
+    "daud": "ali",
+    "shah": "ji",
+    "hi": "kia",
+    "uy": "chawal"
+}
+
+
+daud1={
+    2:5,
+    1:5
+}
+
+print(daud.keys())
+print(daud.values())
+print(daud.items())
+
+print(daud["daud"])
+
+
+
+daud.update(daud1)
+print(daud)
+
+
+
+
+
+print(daud[7])'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a={"daud","ali",6,9,8}
+'''b={"qw","po",3,6,7}
+print(len(a))
+
+w=a.intersection(b)
+print(w)
+'''
+w=(a.pop())
+print(w)
